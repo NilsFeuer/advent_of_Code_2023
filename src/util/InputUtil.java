b1e54fa2a3f3ca3a703d8bd7f2635b24e7eb6582
NilsFeuer <nils_feuer@yahoo.de>
2023-12-17 13:28:05 +0100

    Prep day 10, inputUtil extended

--- src/util/InputUtil.java
+++ src/util/InputUtil.java
@@ -3,6 +3,7 @@ package util;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +15,17 @@ public class InputUtil {
 
         try {
             lines = Files.readAllLines(Paths.get("workspace/src/inputFolder/" + inputFile), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+    public static List<String> readInputLinux(String inputFile) {
+        List<String> lines = new ArrayList<>();
+
+        try {
+            lines = Files.readAllLines(Paths.get("advent_of_code_2023/src/inputFolder/" + inputFile), StandardCharsets.UTF_8);
         } catch (IOException e) {
             e.printStackTrace();
         }
