1f28c8db1d8292a49cf0e7150bba0987b1c29c7c
NilsFeuer <nils_feuer@yahoo.de>
2023-12-22 10:43:20 +0100

    Setup

---
+++ src/day12/HotSprings.java
@@ -0,0 +1,13 @@
+package day12;
+
+import java.util.List;
+import util.InputUtil;
+
+public class HotSprings {
+
+    public static void hotSprings(String inputFile){
+        List<String> input = InputUtil.readInput(inputFile);
+
+
+    }
+}
